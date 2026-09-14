04eecd1cbcbc5c0e8ae3e47d59d056d1dba940bb
Douglas Sampaio <douglassampaio23@gmail.com>
2026-09-14 14:15:37 -0300

    add timestamp para email aleatório

--- automation/cypress/e2e/admSingup.spec.cy.js
+++ automation/cypress/e2e/admSingup.spec.cy.js
@@ -1,13 +1,15 @@
+// cypress/e2e/admSingup.spec.cy.js
 import userData from '../fixtures/users/userData.json'
 import SignupPage from '../pages/signupPage'
 
-
 const signupPage = new SignupPage()
-
 
 describe('TC-UI-001 — Cadastro com sucesso (Administrador)', () => {
   it('cadastro efetuado com sucesso. Usuário redirecionado para a tela de administração e mensagem de boas-vindas exibida.', () => {
+    const timestamp = Date.now()
+    const email = userData.userADM.userEmail.replace('{timestamp}', timestamp)
+
     signupPage.accessSignupPage()
-    signupPage.signupAdmUser(userData.userADM.userName, userData.userADM.userEmail, userData.userADM.userPassword)
+    signupPage.signupAdmUser(userData.userADM.userName, email, userData.userADM.userPassword)
   })
 })
--- automation/cypress/e2e/userSingup.spec.cy.js
+++ automation/cypress/e2e/userSingup.spec.cy.js
@@ -1,13 +1,15 @@
+// cypress/e2e/userSingup.spec.cy.js
 import userData from '../fixtures/users/userData.json'
 import SignupPage from '../pages/signupPage'
 
-
 const signupPage = new SignupPage()
-
 
 describe('registration success', () => {
   it('Deve cadastrar um novo usuário com sucesso', () => {
+    const timestamp = Date.now()
+    const email = userData.user.userEmail.replace('{timestamp}', timestamp)
+
     signupPage.accessSignupPage()
-    signupPage.signupUser(userData.user.userName, userData.user.userEmail, userData.user.userPassword)
+    signupPage.signupUser(userData.user.userName, email, userData.user.userPassword)
   })
 })
