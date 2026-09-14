3c5c8805bb07ea967f7e0c2f3f30bec30d551b6f
Douglas Sampaio <douglassampaio23@gmail.com>
2026-09-14 13:39:18 -0300

    CI fix

--- automation/cypress.config.js
+++ automation/cypress.config.js
@@ -1,18 +1,13 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  projectId: 'ydtsgf',
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
-      // Força saída após os testes
-      on('after:run', () => {
-        process.exit(0)
-      })
+      // implement node event listeners here
     },
+    supportFile: false, // 👈 resolve o erro de vez
     baseUrl: "https://front.serverest.dev/",
     viewportWidth: 1280,
     viewportHeight: 720,
   },
   chromeWebSecurity: false,
   defaultCommandTimeout: 10000,
-})
+};
