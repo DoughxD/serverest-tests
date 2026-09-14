405dbd5e96e4a0212ad8f00f6c27c01b42387ca4
Douglas Sampaio <douglassampaio23@gmail.com>
2026-09-14 14:24:41 -0300

    FIX timestamp: subistituindo o {timestamp} por TIMESTAMP

--- automation/cypress/e2e/admSingup.spec.cy.js
+++ automation/cypress/e2e/admSingup.spec.cy.js
@@ -7,7 +7,7 @@ const signupPage = new SignupPage()
 describe('TC-UI-001 — Cadastro com sucesso (Administrador)', () => {
   it('cadastro efetuado com sucesso. Usuário redirecionado para a tela de administração e mensagem de boas-vindas exibida.', () => {
     const timestamp = Date.now()
-    const email = userData.userADM.userEmail.replace('{timestamp}', timestamp)
+    const email = userData.userADM.userEmail.replace('TIMESTAMP', timestamp)
 
     signupPage.accessSignupPage()
     signupPage.signupAdmUser(userData.userADM.userName, email, userData.userADM.userPassword)
--- automation/cypress/e2e/userSingup.spec.cy.js
+++ automation/cypress/e2e/userSingup.spec.cy.js
@@ -7,7 +7,7 @@ const signupPage = new SignupPage()
 describe('registration success', () => {
   it('Deve cadastrar um novo usuário com sucesso', () => {
     const timestamp = Date.now()
-    const email = userData.user.userEmail.replace('{timestamp}', timestamp)
+    const email = userData.user.userEmail.replace('TIMESTAMP', timestamp)
 
     signupPage.accessSignupPage()
     signupPage.signupUser(userData.user.userName, email, userData.user.userPassword)
